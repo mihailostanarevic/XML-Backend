3c42238c199232b7fc953e2e771126ec796767fb
dimitrije97 <dimitrijesalic@yahoo.com>
2020-06-02 13:28:48 +0200

    Car model maintenance

--- Microservices/car-service/src/main/java/com/rentacar/carservice/repository/ICarModelRepository.java
+++ Microservices/car-service/src/main/java/com/rentacar/carservice/repository/ICarModelRepository.java
@@ -4,9 +4,13 @@ import com.rentacar.carservice.entity.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ICarModelRepository extends JpaRepository<CarModel, UUID> {
 
+    CarModel findOneById(UUID id);
+
+    List<CarModel> findAllByDeleted(boolean deleted);
 }
--- Microservices/car-service/src/main/java/com/rentacar/carservice/service/impl/CarModelService.java
+++ Microservices/car-service/src/main/java/com/rentacar/carservice/service/impl/CarModelService.java
@@ -3,12 +3,14 @@ package com.rentacar.carservice.service.impl;
 import com.rentacar.carservice.dto.request.CreateCarModelRequest;
 import com.rentacar.carservice.dto.request.UpdateCarModelRequest;
 import com.rentacar.carservice.dto.response.CarModelResponse;
+import com.rentacar.carservice.entity.CarModel;
 import com.rentacar.carservice.repository.ICarModelRepository;
 import com.rentacar.carservice.service.ICarModelService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class CarModelService implements ICarModelService {
@@ -21,26 +23,46 @@ public class CarModelService implements ICarModelService {
 
     @Override
     public CarModelResponse createCarModel(CreateCarModelRequest request) throws Exception {
-        return null;
+        CarModel carModel = new CarModel();
+        carModel.setDeleted(false);
+        carModel.setName(request.getName());
+        CarModel savedCarModel = _carModelRepository.save(carModel);
+        return mapCarModelToCarModelResponse(savedCarModel);
     }
 
     @Override
     public CarModelResponse updateCarModel(UpdateCarModelRequest request, UUID id) throws Exception {
-        return null;
+        CarModel carModel = _carModelRepository.findOneById(id);
+        carModel.setName(request.getName());
+        CarModel savedCarModel = _carModelRepository.save(carModel);
+        return mapCarModelToCarModelResponse(savedCarModel);
     }
 
     @Override
     public void deleteCarModel(UUID id) throws Exception {
-
+        CarModel carModel = _carModelRepository.findOneById(id);
+        carModel.setDeleted(true);
+        _carModelRepository.save(carModel);
     }
 
     @Override
     public CarModelResponse getCarModel(UUID id) throws Exception {
-        return null;
+        CarModel carModel = _carModelRepository.findOneById(id);
+        return mapCarModelToCarModelResponse(carModel);
     }
 
     @Override
     public List<CarModelResponse> getAllCarModels() throws Exception {
-        return null;
+        List<CarModel> carModels = _carModelRepository.findAllByDeleted(false);
+        return carModels.stream()
+                .map(carModel -> mapCarModelToCarModelResponse(carModel))
+                .collect(Collectors.toList());
+    }
+
+    private CarModelResponse mapCarModelToCarModelResponse(CarModel carModel) {
+        CarModelResponse response = new CarModelResponse();
+        response.setId(carModel.getId());
+        response.setName(carModel.getName());
+        return response;
     }
 }
