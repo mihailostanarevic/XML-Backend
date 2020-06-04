c80d5b628f3ca5528278292d0ab883ba70723137
dimitrije97 <dimitrijesalic@yahoo.com>
2020-06-04 15:13:55 +0200

    Fixing bugs

--- Microservices/car-service/src/main/resources/data.sql
+++ Microservices/car-service/src/main/resources/data.sql
@@ -75,3 +75,39 @@ insert into fuel_type(id, type, tank_capacity, gas, deleted) values
 ('574930be-7738-48a3-8dc2-9b503b0fe7e3', 'Diesel', '90L', 'true', 'true'),
 ('0dc3ab55-1827-46b6-a2ad-309d83bbd28f', 'Benzine', '90L', 'false', 'false'),
 ('5343655c-07cf-45cf-b911-68fdc33dbf7d', 'Benzine', '90L', 'true', 'true');
+
+--insert into fuelType (type, tankCapacity, gas, deleted) values
+--    ('diesel', '50l', true, false),
+--    ('diesel', '60l', true, false),
+--    ('diesel', '70l', true, false),
+--    ('diesel', '80l', true, false),
+--    ('benzine', '50l', true, false),
+--    ('benzine', '60l', true, false),
+--    ('benzine', '70l', true, false),
+--    ('benzine', '80l', true, false);
+--
+--insert into gearshiftType (type, numberOfGears, deleted) values
+--    ('automatic', 'five'),
+--    ('automatic', 'six'),
+--    ('automatic', 'seven'),
+--    ('automatic', 'eight'),
+--    ('automatic', 'nine'),
+--    ('automatic', 'ten'),
+--    ('manual', 'four'),
+--    ('manual', 'five'),
+--    ('manual', 'six');
+--
+--insert into carClass (name, description, deleted) values
+--    ('SUV', 'Sport utility vehicle (SUV) is a type of automobile that combine elements of road-going passenger cars with features from off-road vehicles, such as raised ground clearance and four-wheel drive.', false),
+--    ('Old Timer', 'A classic car is an older automobile, typically 20 years or older, though definitions vary. The common theme is of an older car of sufficient historical interest to be collectable and worth preserving or restoring rather than scrapping.', false),
+--    ('Coupe', 'A coupé or coupe is a passenger car with a sloping or truncated rear roofline and generally two doors (although several four-door cars have also been marketed as coupés).', false),
+--    ('Sedan', 'Sedans first recorded use as a name for a car body was in 1912. The name comes from a 17th-century development of a litter, the sedan chair, a one-person enclosed box with windows and carried by porters.', false),
+--    ('Hatchback', 'A hatchback is a car with a hatch-type rear door that often opens upwards and often a shared volume for the passenger and cargo areas.', false);
+--
+--insert into carBrand (name, country, deleted) values
+--    ('Jeep', 'USA', false),
+--    ('Ford', 'USA', false),
+--    ('Mazda', 'Japan', false),
+--    ('Fiat', 'Italy', false),
+--    ('Seat', 'Spain', false),
+--    ('Volkswagen', 'Germany', false);
