0db489592d382dfc756d55941c4b3933464c3e7a
dimitrije97 <dimitrijesalic@yahoo.com>
2020-06-23 01:59:56 +0200

    Bugs fixed

--- Microservices/authentification-service/src/main/java/com/rentacar/authentificationservice/controller/UserController.java
+++ Microservices/authentification-service/src/main/java/com/rentacar/authentificationservice/controller/UserController.java
@@ -3,7 +3,6 @@ package com.rentacar.authentificationservice.controller;
 import com.rentacar.authentificationservice.dto.feignClient.UserMessageDTO;
 import com.rentacar.authentificationservice.dto.response.UserResponse;
 import com.rentacar.authentificationservice.services.IUserService;
-import com.rentacar.authentificationservice.util.enums.RequestStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,29 +34,6 @@ public class UserController {
     @GetMapping("/{id}")
     public UserMessageDTO getUser(@PathVariable("id") UUID id){
         return _userService.getUser(id);
-    }
-
-    GetMapping("/{id}/requests")
-    public List<UsersAdsResponse> usersAdsFromStatus(@PathVariable("id") UUID userId, @RequestParam("status") String string){
-        String stringStatus = string.toUpperCase();
-        RequestStatus status;
-        switch (stringStatus){
-            case "PAID" : status = RequestStatus.PAID;
-                break;
-            case "CANCELED" : status = RequestStatus.CANCELED;
-                break;
-            case "CONFIRMED" : status = RequestStatus.CONFIRMED;
-                break;
-            case "DENIED" : status = RequestStatus.DENIED;
-                break;
-            case "APPROVED" : status = RequestStatus.APPROVED;
-                break;
-            case "RESERVED" : status = RequestStatus.RESERVED;
-                break;
-            default: status = RequestStatus.PENDING;
-        }
-
-        return _userService.getUsersRequestFromStatus(userId,status);
     }
 
 }
